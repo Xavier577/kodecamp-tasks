67adc73ad1eaeb3a56d869d8ca718902c240d70c
joseph <josephtsegen10@gmail.com>
2021-10-14 01:04:54 +0100

    completed

--- activity_8_nodejs/src/routes/users.js
+++ activity_8_nodejs/src/routes/users.js
@@ -32,15 +32,69 @@ userRouter.post("/new", (req, res) => {
       (userData) => userData.id === newUser.id
     );
     userWithThatId
-      ? res.json({ message: "user with such id already exists" })
-      : mockData.push(newUser);
+      ? res.json({ message: "user with such id already exists change user id" })
+      : (() => {
+          mockData.push(newUser);
+          res.json({ message: "user sucessfully added" });
+        })();
   } else {
     res.status(404).json({ error: "cannot send empty data" });
   }
 });
 
 // update a user
+userRouter.put("/update/:id", (req, res) => {
+  const { id } = req.params;
+  const userId = parseInt(id);
+  const { firstName, lastName, address } = req.body;
+
+  if (!isNaN(userId)) {
+    const user = mockData.find((user) => user.id === userId);
+    if (firstName || lastName || address) {
+      firstName ? (user.firstName = firstName) : null;
+      lastName ? (user.lastName = lastName) : null;
+      address
+        ? (() => {
+            if (address.country || address.city) {
+              user.address.country
+                ? (user.address.country = address.country)
+                : null;
+              user.address.city ? (user.address.city = address.city) : null;
+            }
+          })()
+        : null;
+
+      res.json({ message: "successfully updated user" });
+    } else {
+      res.json({ message: " cannot update with empty fields" });
+    }
+  } else {
+    res.status(404).json({ error: "id must be an integer" });
+  }
+});
 
 // delete a user
+userRouter.delete("/delete/:id", (req, res) => {
+  const { id } = req.params;
+
+  let userId = parseInt(id);
+
+  if (!isNaN(userId)) {
+    const userIdx = mockData.findIndex((user) => user.id === userId);
+    const user = mockData.find((user) => user.id === userId);
+
+    userIdx > -1
+      ? (() => {
+          mockData.splice(userIdx, 1);
+          res.json({
+            message: `successfully deleted user with id ${id}`,
+            deletedUser: user,
+          });
+        })()
+      : res.json({ message: "no user with such id" });
+  } else {
+    res.send(404).json({ error: "userId required" });
+  }
+});
 
 module.exports = userRouter;
